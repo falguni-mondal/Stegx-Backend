0c2cbb5a3b2002f95efda4a3c869232422138e6c
falguni-mondal <career.falguni@gmail.com>
2025-06-15 13:30:06 +0530

    Updating origins of frontend

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://stegx.netlify.app/'],
+    origin: ['http://localhost:5173', 'https://stegx.netlify.app'],
     credentials: true,
   })
 );
